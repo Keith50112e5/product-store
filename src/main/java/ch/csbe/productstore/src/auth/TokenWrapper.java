16ee3f290634acc7d818954675cd513c7341a5fc
KeithYannickHager <keithyannick.hager01@gmail.com>
2023-09-20 15:22:32 +0200

    TokenWrapper für swagger beschrieben.

--- src/main/java/ch/csbe/productstore/src/auth/TokenWrapper.java
+++ src/main/java/ch/csbe/productstore/src/auth/TokenWrapper.java
@@ -1,10 +1,12 @@
 package ch.csbe.productstore.src.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(description = "DTO für den Token eines Benutzers")
 public class TokenWrapper {
     private String token;
 }
